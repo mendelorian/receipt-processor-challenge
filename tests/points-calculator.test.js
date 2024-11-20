a81550bbb157e953222793b86585d97a4aa6a471
mendelorian <ipanda303@gmail.com>
2024-11-20 08:21:19 +0800

    Scaffold points calculator tests

---
+++ tests/points-calculator.test.js
@@ -0,0 +1,32 @@
+import { describe, it, expect } from 'vitest';
+import { calculatePoints } from '../points-calculator.js';
+
+describe('Points Calculator', () => {
+  it('should award 1 point for each alphanumberic character in retailer name', () => {
+
+  })
+
+  it('should award 50 points if the total is a round dollar amount with no cents', () => {
+
+  })
+
+  it('should award 25 points if the total is a multiple of 0.25', () => {
+
+  })
+
+  it('should award 5 points for every two items on the receipt', () => {
+
+  })
+
+  it('should award (item price * 0.2) points for an item if the trimmed length of the description is multiple of 3', () => {
+
+  })
+
+  it('should award 6 points if the the purchase date day is odd', () => {
+
+  })
+
+  it('should award 10 points if purchase time is between 2pm and 4pm', () => {
+
+  })
+})
